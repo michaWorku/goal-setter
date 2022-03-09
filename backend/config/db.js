591f0a1a0e5f16713f628fbf8d1ad77c787ea04a
michaworku <mikias27worku@gmail.com>
2022-03-09 11:57:50 +0300

    database connection

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = async ()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_LOCAL)
+
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
+    }catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
