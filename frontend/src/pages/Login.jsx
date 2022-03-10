1b5ec3bdce37cb33d1b48dcc1c1912aba6035b8d
michaworku <mikias27worku@gmail.com>
2022-03-10 13:17:38 +0300

    register and login pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
 
 const Login = () => {
+  const [formData, setFormData] = useState({ email:'', password:''})
+
+  const {email, password} = formData
+
+  const onChange = (e)=>{
+    setFormData((prevState)=> setFormData({...prevState, [e.target.name]: e.target.value}))}
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+  }
+  
+
   return (
-    <div>Login</div>
+    <>
+      <section className="heading">
+        <h1><FaSignInAlt/> Login</h1>
+        <p>Login and start settings goals</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="text" name = 'email' id='email' placeholder='Enter your email' value={email} onChange={onChange} className="form-control" />
+          </div>
+          <div className="form-group">
+            <input type="password" name = 'password' id='password' placeholder='Enter your password' value={password} onChange={onChange} className="form-control" />
+          </div>
+          <div className="form-group">
+            <button type='submit' className="btn btn-block"> submit </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaUser } from 'react-icons/fa'
 
 const Register = () => {
+  const [formData, setFormData] = useState({name:'', email:'', password:'', confirmPassword:''})
+
+  const {name, email, password, confirmPassword} = formData
+
+  const onChange = (e)=>{
+    setFormData((prevState)=> setFormData({...prevState, [e.target.name]: e.target.value}))}
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+  }
+  
+
   return (
-    <div>Register</div>
+    <>
+      <section className="heading">
+        <h1><FaUser/> Register</h1>
+        <p>Please crate an account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="text" name = 'name' id='name' placeholder='Enter your name' value={name} onChange={onChange} className="form-control" />
+          </div>
+          <div className="form-group">
+            <input type="text" name = 'email' id='email' placeholder='Enter your email' value={email} onChange={onChange} className="form-control" />
+          </div>
+          <div className="form-group">
+            <input type="password" name = 'password' id='password' placeholder='Enter your password' value={password} onChange={onChange} className="form-control" />
+          </div>
+          <div className="form-group">
+            <input type="password" name = 'confirmPassword' id='confirmPassword' placeholder='Confirm password' value={confirmPassword} onChange={onChange} className="form-control" />
+          </div>
+          <div className="form-group">
+            <button type='submit' className="btn btn-block"> submit </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
