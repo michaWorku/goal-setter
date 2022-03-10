c30c732b08030315fe039ac1da15d0da278864f6
michaworku <mikias27worku@gmail.com>
2022-03-11 00:26:04 +0300

    issue fixed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
+const path = require("path");
 const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
 
